533811235396f634670b4fb84b9532439988d878
Juan Diego wsl <Arenas250421@gmail.com>
2023-08-08 17:16:23 -0500

    backend database

--- backend/public/database/aprendiz.sql
+++ backend/public/database/aprendiz.sql
@@ -1,14 +1,18 @@
-CREATE DATABASE IF NOT EXISTS 'saga';
+CREATE DATABASE IF NOT EXISTS saga;
 
-CREATE TABLE `saga`.`aprendiz` (
-  `aprid` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(255) NOT NULL,
-  `apellido` VARCHAR(255) NOT NULL,
-  `cedula` VARCHAR(255) NOT NULL,
-  `correo` VARCHAR(255) NOT NULL,
-  `celular` VARCHAR(255) NULL,
-  `user` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`aprid`),
-  UNIQUE INDEX `cedula_UNIQUE` (`cedula` ASC) VISIBLE,
-  UNIQUE INDEX `user_UNIQUE` (`user` ASC) VISIBLE);
+CREATE TABLE aprendiz (
+  aprid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(255) NOT NULL,
+  apellido VARCHAR(255) NOT NULL,
+  cedula VARCHAR(255) NOT NULL UNIQUE,
+  correo VARCHAR(255) NOT NULL,
+  celular VARCHAR(255) NULL,
+  user VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO aprendiz (nombre, apellido, cedula, correo, celular, user, password)
+VALUES ('Juan Diego', 'Arenas', '12345678910', 'arenas@saga.com', '32091237', 'diego', '123');
+
+INSERT INTO aprendices( nombre, correo, edad, direccion, ciudad, estado)
+VALUES('Marcos', 'correoexample@gmail.com', 19, 'Calle 12-5', 'Garzón', 1);
