58877c9fd3501c72efb1ae48355889a6ae46461f
Diego <pecioso999@gmail.com>
2023-11-04 22:15:36 -0500

    adelanto home

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import '../styles/HomePage.css'
 import NavBar from '../components/navbar/NavBar'
+import facebook from '../assets/facebook.png'
+import github from '../assets/github.png'
+import youtube from '../assets/youtube.svg'
 
 const HomePage = () => {
     return (
         <div>
             <NavBar />
-            <section style={{ marginTop: '4em' }}>
-                Aqui va algo, aun no lo hemos hecho pero ahi va
+            <section className='bienv' style={{ marginTop: '5em' }}>
+                <h1>Bienvenido a SAGA !</h1>
+                <h3>primer apartado</h3>
+                <div className='redes'> 
+                <a href="https://www.facebook.com/diegofernando.polohome" target={'_blank'}><img src={facebook} /></a>
+                <a href="https://github.com/JuanDiego-Arenas/saga" target={'_blank'}><img src={github} /></a>
+                <a href="https://www.youtube.com/channel/UCN8U1HfruznSOR2Gy-TkHJg" target={'_blank'}><img src={youtube} /></a>
+                <a href="https://www.facebook.com/diegofernando.polohome" target={'_blank'}><img src={facebook} /></a>
+                </div>
+                <div>
+                    segundo apartado
+                </div>
             </section>
         </div>
     );
