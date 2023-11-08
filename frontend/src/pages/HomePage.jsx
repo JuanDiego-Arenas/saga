f81de98bc91c2e0c53ed7096d033d6f027dd4a01
XH0PE <pecioso999@gmail.com>
2023-11-08 09:08:14 -0500

    vs

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,22 +5,42 @@ import facebook from '../assets/facebook.png'
 import github from '../assets/github.png'
 import youtube from '../assets/svg/youtube.svg'
 import portatil from '../assets/portatil.png'
+import HeroImage from '../../public/images/grupo.jpg'
+import HeroImage2 from '../../public/images/image1.webp'
 
 const HomePage = () => {
     return (
         <div>
             <NavBar />
-            <section className='bienv' style={{ marginTop: '4em' }}>
-                <div className='s-info'>
+            <section className='flex flex-col lg:flex-row gap-10 lg:gap-12 mt-16'>
+                <div className="absolute w-full lg:w-1/2 inset-y-0 lg:right-0 ">
+                    <span className="absolute -left-6 md:left-4 top-24 lg:top-28 w-24 h-24 rotate-90 skew-x-12 rounded-3xl bg-green-400 blur-xl opacity-60 lg:opacity-95 lg:block hidden"></span>
+                    <span className="absolute right-4 bottom-12 w-24 h-24 rounded-3xl bg-primary blur-xl opacity-80"></span>
                 </div>
-                <h1>Bienvenido a SAGA !</h1>
-                <div><h3>Seguimos trabajando para poder presentar un proyecto y solucion agradable a la vista</h3></div>
-                <div className='img'><img src={portatil} /></div>
+                <span className="w-4/12 lg:w-2/12 aspect-square bg-gradient-to-tr from-primary to-green-400 absolute -top-5 lg:left-0 rounded-full skew-y-12 blur-2xl opacity-40 skew-x-12 rotate-90"></span>
+                <div className=" flex flex-col justify-center  text-center lg:text-left lg:py-7 xl:py-8 
+                                    lg:items-start lg:max-w-none max-w-3xl mx-auto lg:mx-0 lg:flex-1 lg:w-1/2" style={{ alignItems: 'center' }}>
+                    <h1 className="text-3xl/tight sm:text-4xl/tight md:text-5xl/tight xl:text-6xl/tight mt-5
+                        font-bold" style={{ textAlign: 'left !important' }}>
+                        Bienvenidos a <span style={{ color: '#39a900', width: 'fit-content' }}>SAGA</span> 
+
+                        <br/>
+                    <span className="text-transparent bg-clip-text bg-gradient-to-br " style={{ color: '39a900' }}>SAGA</span> El Futuro del SENA.
+                    </h1>
+                    <p className="mt-8 max-w-screen-sm p-3 text-xl">
+                        SAGA es un proyecto formativo, con el objetivo de lograr una mejor gestión en la toma de asistencias, también cuenta con un apartado de noticias y prestamos para la biblioteca</p>
+                    <div className="mt-10  w-full flex max-w-md mx-auto lg:mx-0">
+                        <div className="flex sm:flex-row flex-col gap-5 w-full">
+
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-1 lg:w-1/2 lg:h-auto relative lg:max-w-none lg:mx-0 mx-auto max-w-3xl">
+                    <img src={HeroImage} />
+                </div>
             </section>
-            <section className='more'>
-                <div>
-                    <h1>Aqui va la segunda seccion</h1>
-                </div>
+            <section className='flex flex-col lg:flex-row gap-10 lg:gap-12'>
+                    <img src={HeroImage2} />
             </section>
         </div>
     );
