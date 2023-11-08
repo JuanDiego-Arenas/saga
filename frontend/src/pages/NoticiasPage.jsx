650f5d042083c356e2552b04d2ee0d469ec670cf
JuanferGG <juanfe15777@gmail.com>
2023-11-08 11:01:03 -0500

    v.09 rol aprendiz navBar

--- frontend/src/pages/NoticiasPage.jsx
+++ frontend/src/pages/NoticiasPage.jsx
@@ -6,10 +6,12 @@ const NoticiasPage = () => {
     return (
         <>
             <NavBar />
-            <Tweet id="941451838032875520" />
-            <Tweet id="944247851630112768" />
-            <Tweet id="1696588332589818082" />
-            <Tweet id="1445103163971883012" />
+            <section className='mt-24'>
+                <Tweet id="941451838032875520" />
+                <Tweet id="944247851630112768" />
+                <Tweet id="1696588332589818082" />
+                <Tweet id="1445103163971883012" />
+            </section>
         </>
     );
 }
