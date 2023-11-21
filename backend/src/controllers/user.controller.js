4e2d263d6b810d31f6d2ca7fb8e6bbb7f439ac12
Juan Diego Win <Arenas250421@gmail.com>
2023-11-20 23:04:13 -0500

    updateUser terminado

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,8 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 export const getMe = async (req, res) => {
 	const { id } = req.user;
@@ -18,9 +21,13 @@ export const getUsers = async (req, res) => {
 	res.status(200).json({ data });
 };
 
-export const updateUser = async (req, res) => {
+export const updateUser = async (req, res, next) => {
 	const { cedula } = req.params;
 	const userData = req.body;
+
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = path.dirname(__filename);
+	// return console.log(__dirname);
 
 	try {
 		// Password
@@ -34,25 +41,22 @@ export const updateUser = async (req, res) => {
 
 		// Avatar
 		if (req.files) {
-			const newsFound = await Noticia.findOne({ _id: id });
-			const file = req.files.image;
+			const userFound = await User.findOne({ cc: cedula });
+			const file = req.files.avatar;
 
 			// Eliminar la imagen anterior si existe
-			if (newsFound.image) {
-				const oldImageFullPath = path.resolve(__dirname, '..', 'uploads', newsFound.image);
-				fs.unlinkSync(oldImageFullPath);
+			if (userFound.avatar !== 'avatars/userdefault.jpg') {
+				try {
+					const oldImageFullPath = path.resolve(__dirname, '..', 'uploads', userFound.avatar);
+					fs.unlinkSync(oldImageFullPath);
+				} catch (error) {}
 			}
 
-			const titlePath = newsFound.title
-				.split('')
-				.map(e => (e === ' ' ? '-' : e))
-				.join('');
-
-			const rutaArchivoRelativa = path.join('uploads', 'notices', titlePath, file.name);
-			const rutaSinUpload = path.join('notices', titlePath, file.name);
+			const rutaArchivoRelativa = path.join('uploads', 'avatars', `${userFound.cc}_${file.name}`);
+			const rutaSinUpload = path.join('avatars', `${userFound.cc}_${file.name}`);
 
 			fs.writeFileSync(path.resolve(__dirname, '..', rutaArchivoRelativa), file.data);
-			newsData.image = rutaSinUpload;
+			userData.avatar = rutaSinUpload;
 		}
 
 		const updatedUser = await User.findOneAndUpdate({ cc: cedula }, userData, {
@@ -65,6 +69,7 @@ export const updateUser = async (req, res) => {
 
 		return res.status(200).send({ msg: 'Datos actualizados (～￣▽￣)～' });
 	} catch (error) {
+		console.log(error);
 		return res.status(500).json({ msg: error.message });
 	}
 };
