9d32fae3d490dccb1d8f6ee49dc79936e2bcf895
XH0PE <pecioso999@gmail.com>
2023-11-23 10:45:42 -0500

    Footer V2 Enlaces/Iconos/Equipo-Saga

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,8 +3,12 @@ import '../styles/HomePage.css';
 import NavBar from '../components/navbar/NavBar';
 import HeroImage from '../../public/images/grupo.jpg';
 import LogoSena from '../assets/svg/LogoSena.svg';
-import Check from '../assets/svg/check.svg';
 import Saga from '../assets/svg/SAGA.svg'
+import Check from '../assets/svg/check.svg';
+import facebook from '../assets/svg/facebook.svg'
+import youtube from '../assets/svg/youtube.svg'
+import twitter from '../assets/svg/twitter.svg'
+import instagram from '../assets/svg/instagram.svg'
 
 const HomePage = () => {
   return (
@@ -41,7 +45,7 @@ const HomePage = () => {
             <img
               src={HeroImage}
               className='lg:absolute lg:w-full lg:h-full rounded-3xl object-cover lg:max-h-none max-h-96'
-              style={{ width: '2350px' }}
+              style={{width: '45vw', marginRight: '30px'}}
               alt='Hero'
             />
           </div>
@@ -67,7 +71,7 @@ const HomePage = () => {
                   <br />
                   <br /> SAGA simplifica la toma de asistencias y la gestión de eventos, lo que significa menos tiempo invertido en tareas administrativas tediosas y más tiempo dedicado a lo que realmente importa: la educación.
                 </li>
-                <li class="benefits">
+                <li className="benefits">
                     
                     <span className="font-bold bg-box-bg rounded-full w-30 h-8 mr-3 text-primary inline-flex justify-center items-center">
                     <img src={Check} /><h1 style={{marginLeft: '10px', fontSize: '20px'}}>Mejora en la Toma de Decisiones:</h1>
@@ -77,7 +81,7 @@ const HomePage = () => {
                     Con acceso a datos precisos en tiempo real, los líderes del SENA pueden tomar decisiones informadas para mejorar la eficiencia y la calidad de la formación.
                     
                 </li>
-                <li class="benefits">
+                <li className="benefits">
                     
                     <span className="font-bold bg-box-bg rounded-full w-30 h-8 mr-3 text-primary inline-flex justify-center items-center"><img src={Check} /><h1 style={{marginLeft: '10px', fontSize: '20px'}}>Reducción de Errores Administrativos:</h1>
                     </span>  
@@ -86,7 +90,7 @@ const HomePage = () => {
                     SAGA elimina la posibilidad de errores humanos en la toma de asistencias, lo que garantiza un registro preciso y confiable de las actividades de los aprendices.
                     
                 </li>
-                <li class="benefits">
+                <li className="benefits">
                     
                     <span className="font-bold bg-box-bg rounded-full w-30 h-8 mr-3 text-primary inline-flex justify-center items-center"><img src={Check} /><h1 style={{marginLeft: '10px', fontSize: '20px'}}>Facilita la Comunicación:</h1></span>  
                     <br/>
@@ -176,7 +180,7 @@ const HomePage = () => {
       </div>
 
 <section className="mt-16" style={{width: '100vw'}}>
-<footer className="relative bg-gradient-to-tr from-gray-200 to-gray-200 dark:from-gray-400 pt-28 rounded-t-3xl">
+<footer className="relative bg-gradient-to-tr from-gray-200 to-gray-300 dark:from-gray-400 pt-28 rounded-t-3xl">
     <div className="absolute right-0 top-0 h-full w-full flex justify-end">
         <div className="w-28 h-28 overflow-hidden flex rounded-xl relative blur-2xl">
             <span
@@ -204,7 +208,7 @@ const HomePage = () => {
             ></span>
           </div>
       </div>
-    <div className="pb-5 relative overflow-hidden mx-auto max-w-7xl w-full px-5 sm:px-8 md:px-14 lg:px-5">
+    <div className="pb-5 relative overflow-hidden mx-auto max-w-7xl w-full px-5 sm:px-8 md:px-14 lg:px-5" style={{marginBottom: '50px'}}>
         
         <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 blur-2xl opacity-20 w-24 h-16 sm:w-48 sm:h-36 rounded-full rotate-12 skew-x-6 bg-primary"></span>
         <div className="grid grid-cols-2 lg:grid-cols-3 lg:items-stretch gap-8 relative">
@@ -212,43 +216,44 @@ const HomePage = () => {
                 <div className="h-full">
                     <a href="https://oferta.senasofiaplus.edu.co/sofia-oferta/" className="relative flex items-center gap-1" target='_blank'>
                         <div className="relative w-10 h-10 overflow-hidden flex rounded-xl">
-                            <img src={Saga} />
+                            <img src={LogoSena} />
                         </div>
                         <div className="inline-flex text-lg font-semibold text-heading-1">
-                            SAGA
+                            SENA
                         </div>
                     </a>
                     <p className="mt-8 md:text-lg text-heading-3">
-                        “Sé bueno con los nerds. Es muy probable que termines trabajando para uno de ellos” - Bill Gates
+                        ❝Una entidad de clase mundial❞
                     </p>
                 </div>
 
+                <div style={{display: 'flex', width: '100px', position: 'absolute', left: '65vw'}}>
+                  <ul>
+                  <h1 style={{fontWeight: '600', fontSize: '25px'}}>Equipo</h1>
+                    <li><a href='https://github.com/JuanferGG' target='_blank'>JuanferGG</a></li>
+                    <li><a href='https://github.com/JuanDiego-Arenas' target='_blank'>Juan Diego</a></li>
+                    <li><a href='https://github.com/XH0PE' target='_blank'>Diego Polo</a></li>
+                  </ul>
+                </div>
+
                 <div className="min-h-max flex items-center gap-4 text-heading-3 mt-8">
-                    <a href="https://www.facebook.com/diegofernando.polohome/" target="_blank" className="transition hover:text-heading-1 hover:scale-105">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
-                            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
-                        </svg>
+                    <a href="https://web.facebook.com/SENA/?_rdc=1&_rdr" target="_blank" className="transition hover:text-heading-1 hover:scale-105">
+                        <img src={facebook}/>
                         <span className="sr-only">social link</span>
                     </a>
 
-                    <a href="#" className="transition hover:text-heading-1 hover:scale-105">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
-                            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-                        </svg>
+                    <a href="https://www.youtube.com/@SENAComunica" target='_blank' className="transition hover:text-heading-1 hover:scale-105">
+                        <img src={youtube}/>
                         <span className="sr-only">social link</span>
                     </a>
 
-                    <a href="https://twitter.com/ekisjope" target="_blank" className="transition hover:text-heading-1 hover:scale-105">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
-                            <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
-                        </svg>
+                    <a href="https://twitter.com/SENAComunica" target="_blank" className="transition hover:text-heading-1 hover:scale-105">
+                        <img src={twitter}/>
                         <span className="sr-only">social link</span>
                     </a>
 
                     <a href="https://www.facebook.com/diegofernando.polohome/" target="_blank" className="transition hover:text-heading-1 hover:scale-105">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
-                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-                        </svg>
+                        <img src={instagram}/>
                         <span className="sr-only">social link</span>
                     </a>
                 </div>
