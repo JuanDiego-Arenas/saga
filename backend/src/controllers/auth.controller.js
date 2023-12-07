688a7d6373892072cb3e50d95f04a2812fdc27e5
Juan Diego Win <Arenas250421@gmail.com>
2023-12-07 15:23:16 -0500

    token

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -148,13 +148,14 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-	res.cookie('token', '', {
-		path: '/',
-		httpOnly: true,
-		expires: new Date(0), //expires 1 day
-		sameSite: 'none',
-		secure: true,
-	});
+	res.cookie('token', '');
+	// res.cookie('token', '', {
+	// 	path: '/',
+	// 	httpOnly: true,
+	// 	expires: new Date(0), //expires 1 day
+	// 	sameSite: 'none',
+	// 	secure: true,
+	// });
 	return res.status(200).json({ msg: 'Logout' });
 };
 
